38582bbaef22e598cf1096e048a68a8ce1e129d7
unknown <wilsonsantana100>
2022-06-06 23:31:56 -0300

    inicial app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootswatch/dist/flatly/bootstrap.css'
+import './custom.css'
+
+import Login from './views/login';
+import CadastroUsuario from './views/cadastroUsuario';
+
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <CadastroUsuario />
+    
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default App
+
+
+
---
+++ src/views/cadastroUsuario.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import Card from '../components/card'
+import FormGroup from '../components/form-group';
+
+class CadastroUsuario extends React.Component {
+       
+    state = {
+        nome: '',
+        email: '',
+        senha: '',
+        senhaRepeticao: ''
+    }
+
+    cadastrar = () => {
+        console.log(this.state)
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <Card title="Cadastro de Usuário">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="bs-component">
+                                
+                                <FormGroup label="Nome: *" htmlFor="inputNome">
+                                    <input type="text"
+                                            id="inputNome"
+                                            className="form-control"
+                                            name="nome"
+                                            onChange={e => this.setState({nome: e.target.value})} />
+                                </FormGroup>
+
+                                <FormGroup label="Email: *" htmlFor="inputEmail">
+                                    <input type="email"
+                                            id="inputEmail"
+                                            className="form-control"
+                                            name="email"
+                                            onChange={e => this.setState({email: e.target.value})} />
+                                </FormGroup>
+
+                                <FormGroup label="Senha: *" htmlFor="inputSenha">
+                                    <input  type="password"
+                                            id="inputSenha"
+                                            className="form-control"
+                                            name="senha"
+                                            placeholder="Password" 
+                                            onChange={e => this.setState({senha: e.target.value})} />
+                                </FormGroup>
+
+                                <FormGroup label="Confirmar Senha: *" htmlFor="inputsenhaRepeticao">
+                                    <input  type="password"
+                                            id="inputsenhaRepeticao"
+                                            className="form-control"
+                                            name="senhaRepeticao"
+                                            placeholder="Password" 
+                                            onChange={e => this.setState({senhaRepeticao: e.target.value})} />
+                                </FormGroup>
+
+                                <button  onClick={this.cadastrar} type="button" className="btn btn-success">Salvar</button>
+                                <button  type="button" className="btn btn-danger">Cancelar</button>
+
+                            </div>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+
+        )
+    }
+}
+
+export default CadastroUsuario
