5605235d94ac61bdc6371a642b620b46f5cb55a3
unknown <wilsonsantana100>
2022-06-15 01:20:26 -0300

    dia 15/06 - 01:20

--- src/views/login.js
+++ src/views/login.js
@@ -2,17 +2,28 @@ import React from 'react';
 import Card from '../components/card';
 import FormGroup from '../components/form-group';
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 
 class Login extends React.Component {
 
     state = {
         email: '',
-        senha: ''
+        senha: '',
+        mensagemErro: null
     }
     
     entrar = () => {
-        console.log('Email: ', this.state.email)
-        console.log('Senha: ', this.state.senha)
+       axios
+       .post('http://localhost:8080/api/usuarios/autenticar', {
+            email: this.state.email,
+            senha: this.state.senha
+       }).then( response => {
+            this.props.history.push('/home')
+       }).catch( erro => {
+            this.setState({mensagemErro: erro.response.data})
+       })
+
+       
     }
     
     prepareCadastrar = () => {
@@ -27,6 +38,10 @@ class Login extends React.Component {
                     <div className="bs-docs-section">
                         <Card title="Login">
                            
+                           <div className="row">
+                              <span>{this.state.mensagemErro}</span>
+                           </div>
+
                            <div className="row">
                                 <div className="col-lg-12">
                                     <div className="bs-component">
